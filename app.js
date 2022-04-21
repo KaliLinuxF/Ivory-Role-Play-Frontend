d10cf902c810bb3d594fc1eddc02827090a947aa
elozxzx <elozxzx@gmail.com>
2022-04-21 22:00:24 +0300

    commit
    design improvements
    added password recovery page

--- app.js
+++
@@ -1,5 +0,0 @@
-function auth() {
-    const form = document.getElementById('formLogin');
-    mp.trigger('client.userLogin',  JSON.stringify({login: form.elements.login.value, 
-                                                    password: form.elements.password.value}));
-}
--- js/app.js
+++ js/app.js
@@ -38,3 +38,39 @@ function setError(field, state) {
 
     console.log(error.style.visibility);
 }
+
+// Password Recovery Section
+
+function OTPInput() {
+    const inputs = document.querySelectorAll('#otp > *[id]');
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('keydown', function(event) {
+        if (event.key === "Backspace") {
+          inputs[i].value = '';
+          if (i !== 0)
+            inputs[i - 1].focus();
+        } else {
+          if (i === inputs.length - 1 && inputs[i].value !== '') {
+            return true;
+          } else if (event.keyCode > 47 && event.keyCode < 58) {
+            inputs[i].value = event.key;
+            if (i !== inputs.length - 1)
+              inputs[i + 1].focus();
+            event.preventDefault();
+          } else if (event.keyCode > 64 && event.keyCode < 91) {
+            inputs[i].value = String.fromCharCode(event.keyCode);
+            if (i !== inputs.length - 1)
+              inputs[i + 1].focus();
+            event.preventDefault();
+          }
+        }
+      });
+    }
+  }
+OTPInput();
+
+/* 
+onclick = recovery()
+OTP input id's = OTP1, OTP2, OTP3, OTP4, OTP5, OTP6
+form id = "passRecovery"
+*/
